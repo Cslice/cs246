c17dfa6f3a3dcdeeeb71bd4510874e710dd6d3a7
Cameron Thomas <cameron@Camerons-MacBook-Pro.local>
2014-05-15 22:37:45 -0600

    next2

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,5 +1,6 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
-	System.out.println("Hello GetHub");
+	System.out.println("Hello GetHub World");
+	System.out.println("Testing...");
     }
 }
