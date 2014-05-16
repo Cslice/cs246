08b327df4d42b706bc468d6086d7436d419a9213
Cameron Thomas <cameron@Camerons-MacBook-Pro.local>
2014-05-15 22:27:55 -0600

    fix2

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,10 +1,5 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("Hello GetHub");
-=======
-	System.out.println("Hello GetHub World");
-	System.out.println("Testing...");
->>>>>>> test
     }
 }
